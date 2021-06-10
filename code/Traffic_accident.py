53e46a73f3d05bd7654720f022a22a56d5a4564e
YoshikiHanashiro <e195733@ie.u-ryukyu.ac.jp>
2021-06-10 15:42:47 +0900

    エラーは出ないけど結果がいまいち成功してるかわからない再帰学習コード

--- code/Traffic_accident.py
+++ code/Traffic_accident.py
@@ -1,38 +1,50 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn import svm
+from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
 
-#読み込んだ元データを、データとラベルに分割する。今回はデータ数が多い白ワインを使用する。
-wine = pd.read_csv('data_mining/Experiment_datamining/G1/group1_datamining/dataset/honhyo.csv',sep=',')
-label = wine['quality']
-data = wine.drop(columns = 'quality')
 
-# 用意したデータ各列の平均と標準偏差を確認
-print('元データの平均：'+ str(np.mean(data.values[:, :])))
-print('元データの標準偏差：'+ str(np.std(data.values[:, :])))
- 
-# 標準化を行う
-sc = StandardScaler()
-std = sc.fit_transform(data)
+#読み込んだ元データを、データとラベルに分割する。
+accident = pd.read_csv('data-mining/Experiment_datamining/G1/group1_datamining/dataset/accident_okinawa.csv', sep=',')
+label = accident['count']
+data = accident.iloc[:,4:5]
 
-print('標準化後の平均：'+ str(np.mean(std[:, :])))
-print('標準化後の標準偏差：'+ str(np.std(std[:, :])))
 
 #データを学習用とテスト用に分割する。今回はデータの半数を学習用とする。
 data_train,data_test,label_train,label_test = train_test_split(data,label,test_size = 0.5)
-data_std_train,data_std_test = train_test_split(std,test_size = 0.5)
 
 #データをLinearSVCを使用して学習する。
-clf = svm.LinearSVC()
+clf = SGDRegressor(max_iter=1000)
 clf.fit(data_train,label_train)
 
 #学習したモデルを使用して精度を確認する。
-test_pred = clf.predict(data_test)
-print('正答率は'+ str(accuracy_score(label_test,test_pred) * 100) + '%です。(LinearSVC)')
-print('分類結果(評価段階は0~10ですが実際には3~9までの評価しかないため7*7行列となります。)')
-print(confusion_matrix(label_test,test_pred))
+
+#解説 6：結果をプロットする------------------------------------------------
+
+line_X=np.arange(-4, 4, 0.1) #3から10まで1刻み
+line_Y=clf.predict(line_X[:, np.newaxis])
+fig = plt.figure(figsize=(10,10))
+plt.subplot(2, 1, 1)
+plt.scatter(data_train, label_train, c='b', marker='s')
+plt.plot(line_X, line_Y, c='r')
+plt.show
+fig.savefig("data-mining/Experiment_datamining/G1/group1_datamining/result/img1.png")
+
+#解説 7：誤差をプロットする-------------------------------------------------
+Y_rm_pred=clf.predict(data_test)
+plt.subplot(2, 1, 2)
+plt.scatter(label_test, Y_rm_pred-label_test, c='b', marker='s', label="RM_only")
+
+Y_pred=clf.predict(data_test)
+plt.scatter(label_test, Y_pred-label_test, c='r', marker='s',label="ALL")
+plt.legend()
+plt.hlines(y=0, xmin=0, xmax=50, colors='black')
+plt.show
+fig.savefig("data-mining/Experiment_datamining/G1/group1_datamining/result/img2.png")
+
+print("\n平均2乗誤差")
+RMS=np.mean((Y_pred - label_test) ** 2)
+print(RMS)
