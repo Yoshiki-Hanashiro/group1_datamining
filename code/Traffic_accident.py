f5d7e73360118bd006e1d7238a92936bd949a297
YoshikiHanashiro <e195733@ie.u-ryukyu.ac.jp>
2021-06-17 14:56:09 +0900

    出力と処理部分のコードを訂正

--- code/Traffic_accident.py
+++ code/Traffic_accident.py
@@ -1,3 +1,4 @@
+from numpy import testing
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -9,9 +10,10 @@ import matplotlib.pyplot as plt
 
 #読み込んだ元データを、データとラベルに分割する。
 accident = pd.read_csv('data-mining/Experiment_datamining/G1/group1_datamining/dataset/accident_okinawa.csv', sep=',')
+accident = accident.dropna()
 label = accident['count']
-data = accident.iloc[:,4:5]
-
+data = accident.iloc[:,5:7]
+print(accident.isnull().sum())
 
 #データを学習用とテスト用に分割する。今回はデータの半数を学習用とする。
 data_train,data_test,label_train,label_test = train_test_split(data,label,test_size = 0.5)
@@ -21,7 +23,16 @@ clf = SGDRegressor(max_iter=1000)
 clf.fit(data_train,label_train)
 
 #学習したモデルを使用して精度を確認する。
+test_pred = clf.predict(data_test)
+difference = np.mean(test_pred - label_test)
+print(difference)
 
+fig = plt.figure(figsize=(10,10))
+plt.scatter(data['temperature'], data['rain'], marker = '.')
+fig.savefig("data-mining/Experiment_datamining/G1/group1_datamining/result/img2.png")
+
+
+"""
 #解説 6：結果をプロットする------------------------------------------------
 
 line_X=np.arange(-4, 4, 0.1) #3から10まで1刻み
@@ -38,8 +49,6 @@ Y_rm_pred=clf.predict(data_test)
 plt.subplot(2, 1, 2)
 plt.scatter(label_test, Y_rm_pred-label_test, c='b', marker='s', label="RM_only")
 
-Y_pred=clf.predict(data_test)
-plt.scatter(label_test, Y_pred-label_test, c='r', marker='s',label="ALL")
 plt.legend()
 plt.hlines(y=0, xmin=0, xmax=50, colors='black')
 plt.show
@@ -48,3 +57,4 @@ fig.savefig("data-mining/Experiment_datamining/G1/group1_datamining/result/img2.
 print("\n平均2乗誤差")
 RMS=np.mean((Y_pred - label_test) ** 2)
 print(RMS)
+"""
