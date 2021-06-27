73cd19eb10befcc50859e41e6a750ec1d249de16
Hiroki SHIBATA <e195726@ie.u-ryukyu.ac.jp>
2021-06-27 15:53:52 +0900

    3次元で回帰平面をプロットするコード

--- code/Traffic_accident.py
+++ code/Traffic_accident.py
@@ -6,10 +6,11 @@ from sklearn.linear_model import SGDRegressor
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 #読み込んだ元データを、データとラベルに分割する。
-accident = pd.read_csv('data-mining/Experiment_datamining/G1/group1_datamining/dataset/accident_okinawa.csv', sep=',')
+accident = pd.read_csv('group1_datamining/dataset/accident_okinawa.csv', sep=',')
 accident = accident.dropna()
 label = accident['count']
 data = accident.iloc[:,5:7]
@@ -38,7 +39,45 @@ plt.title("Traffic_Accident")
 plt.xlabel("temperature")
 plt.ylabel("rain")
 plt.grid(True)
-fig.savefig("data-mining/Experiment_datamining/G1/group1_datamining/result/img2.png")
+fig.savefig("group1_datamining/result/img2.png")
+
+#結果をプロットする(回帰平面)------------------------------------------------
+# パラメータ算出
+reg_wn = clf.coef_ # 偏回帰係数
+reg_w0 = clf.intercept_ # 切片
+r2 = clf.score(data_test, label_test) # 決定係数
+print(reg_wn)
+print(reg_w0)
+print(r2)
+ 
+# パラメータからモデルを可視化するために3次元データを作成する
+X1 = np.arange(0, 35, 5.0) # x軸を作成
+X2 = np.arange(0, 50, 5.0) # y軸を作成
+X, Y = np.meshgrid(X1, X2) # x軸とy軸からグリッドデータを作成
+Z = reg_w0 + (reg_wn[0] * X) + (reg_wn[1] * Y) # 回帰平面のz値を作成
+ 
+# ここからグラフ描画
+# フォントの種類とサイズを設定する。
+plt.rcParams['font.size'] = 14
+plt.rcParams['font.family'] = 'Times New Roman'
+ 
+# グラフの入れ物を用意する。
+fig = plt.figure()
+ax1 = Axes3D(fig)
+ 
+# 軸のラベルを設定する。
+ax1.set_xlabel('temperature')
+ax1.set_ylabel('rain')
+ax1.set_zlabel('Traffic_Accident')
+ 
+# データプロットする。
+ax1.scatter3D(data_test['temperature'], data_test['rain'], label_test, label='Dataset')
+ax1.plot_wireframe(X, Y, Z, label='Regression plane', color = 'red')
+plt.legend()
+ 
+# グラフを表示する。
+plt.show()
+fig.savefig("group1_datamining/result/img3.png")
 
 
 """
@@ -51,7 +90,7 @@ plt.subplot(2, 1, 1)
 plt.scatter(data_train, label_train, c='b', marker='s')
 plt.plot(line_X, line_Y, c='r')
 plt.show
-fig.savefig("data-mining/Experiment_datamining/G1/group1_datamining/result/img1.png")
+fig.savefig("group1_datamining/result/img1.png")
 
 #解説 7：誤差をプロットする-------------------------------------------------
 Y_rm_pred=clf.predict(data_test)
@@ -61,7 +100,7 @@ plt.scatter(label_test, Y_rm_pred-label_test, c='b', marker='s', label="RM_only"
 plt.legend()
 plt.hlines(y=0, xmin=0, xmax=50, colors='black')
 plt.show
-fig.savefig("data-mining/Experiment_datamining/G1/group1_datamining/result/img2.png")
+fig.savefig("group1_datamining/result/img2.png")
 
 print("\n平均2乗誤差")
 RMS=np.mean((Y_pred - label_test) ** 2)
